1bb4a04f2e58a75b13aeafa9754216cc91f02fdd
Denis Bazhenov <bazhenov@farpost.com>
2019-06-30 21:09:46 +1000

    Some work on cli reporting

--- serve/src/main.rs
+++ serve/src/main.rs
@@ -1,7 +1,7 @@
 extern crate tensorflow;
 extern crate encoding;
 
-use tensorflow::{Graph, Session, SessionOptions, SessionRunArgs, Tensor};
+use tensorflow::{Graph, Session, SessionOptions, SessionRunArgs, Tensor, Operation};
 use std::fs::File;
 use std::io::Read;
 use std::error::Error;
@@ -20,10 +20,31 @@ fn main() {
 	})
 }
 
+fn tensor_from_str(string: &str) -> Result<Tensor<f32>, Box<dyn Error>> {
+	let len = string.chars().count();
+
+	let mut tensor = Tensor::new(&[1, len as u64]);
+	for (i, c) in string.chars().enumerate() {
+		let r = WINDOWS_1251.encode(&c.to_string(), EncoderTrap::Strict)?;
+		tensor[i] = r[0] as f32;
+	}
+	Ok(tensor)
+}
+
+fn session_run(session: &Session, input: &Operation, input_tensor: &Tensor<f32>,
+							 output: &Operation) -> Result<Tensor<f32>, Box<dyn Error>> {
+	let mut run_args = SessionRunArgs::new();
+
+	run_args.add_feed(&input, 0, &input_tensor);
+	let output_token = run_args.request_fetch(&output, 0);
+
+	session.run(&mut run_args)?;
+
+	Ok(run_args.fetch::<f32>(output_token)?)
+}
+
 fn run() -> Result<(), Box<dyn Error>> {
-	let mut proto = Vec::new();
-	File::open("../private/model.pb")?.read_to_end(&mut proto)?;
-	let export_dir = "../private/model/1561464809";
+	let export_dir = "../private/model/1561865486";
 
 	let mut graph = Graph::new();
 	let tags: Vec<&str> = vec!["serve"];
@@ -34,45 +55,34 @@ fn run() -> Result<(), Box<dyn Error>> {
 		export_dir,
 	)?;
 
-	if let Ok(devices) = session.device_list() {
-		for ref d in devices {
-			println!("{}", d.device_type);
-			println!("{}", d.name);
-		}
-	}
-
-	let input = graph.operation_by_name_required("embedding_input")?;
-	let output = graph.operation_by_name_required("flatten/Reshape")?;
-
-	let mut input_tensor = Tensor::new(&[1, 64]).with_values(&[0f32; 64]).unwrap();
+	let input = graph.operation_by_name_required("input")?;
+	let output = graph.operation_by_name_required("output/Reshape")?;
 
 	let text = String::from("Привет, перезвоните мне на +7(907-8O57113 скорее");
-	for (i, c) in text.chars().enumerate().take(64) {
-		let r = WINDOWS_1251.encode(&c.to_string(), EncoderTrap::Strict)?;
-		input_tensor[i] = r[0] as f32;
+
+	let indices = text.char_indices().collect::<Vec<_>>();
+
+	let chunk_size = 32;
+
+	for chunk in indices.chunks(chunk_size) {
+		let (from, _) = *chunk.first().unwrap();
+		let (to, _) = *chunk.last().unwrap();
+		let chunk_str = chunk.iter()
+			.map(|i| i.1)
+			.collect::<String>();
+		let input_tensor = tensor_from_str(&chunk_str)?;
+		let output_tensor = session_run(&session, &input, &input_tensor, &output)?;
+		let mut vector = vec![0f32; chunk_size];
+		for i in 0..chunk.len() {
+			vector[i] = output_tensor[i];
+		}
+		let mask_str = vector.iter()
+			.map(|i| if *i >= 0.5 { '^' } else { ' ' })
+			.collect::<String>();
+		println!("{}", chunk_str);
+		println!("{}", mask_str);
 	}
 
-	let mut run_args = SessionRunArgs::new();
-
-	run_args.add_feed(&input, 0, &input_tensor);
-	let output_token = run_args.request_fetch(&output, 0);
-
-	session.run(&mut run_args)?;
-
-	let size = 64;
-	let tensor = run_args.fetch::<f32>(output_token)?;
-	
-	let mut vector = vec![0f32; size];
-	for i in 0..size {
-		vector[i] = tensor[i];
-	}
-
-	for (start, stop) in get_indicies(vector, 0.5) {
-		let fragment = text.chars().skip(start).take(stop - start).collect::<String>();
-		println!(" - {}", fragment);
-	}
-
-	println!("");
 	Ok(())
 }
 
